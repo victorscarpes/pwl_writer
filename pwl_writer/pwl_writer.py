45fd2f7f2f031e43e23d5c8ac481b7879c55449d
victorscarpes <victorscarpes@gmail.com>
2023-06-20 19:56:56 +0200

    Started pycco documentation

--- pwl_writer/pwl_writer.py
+++ pwl_writer/pwl_writer.py
@@ -1,52 +1,18 @@
+"""
+This module defines a class `PWL` to generate objects 
+"""
+
+
 from numbers import Real
-import numpy  # type: ignore
+import numpy as np  # type: ignore
 from typing import Optional, List,  Dict, Callable
 
 
 class PrecisionError(Exception):
-    pass
+    """== PrecisionError *(Exception Class)*==
 
-
-def _exp_edge_func(tau: float, t1: float, f1: float, t2: float, f2: float) -> Callable[[float], float]:
-
-    A = (f1*numpy.exp(t1/tau) - f2*numpy.exp(t2/tau)) / \
-        (numpy.exp(t1/tau) - numpy.exp(t2/tau))
-    B = (f1 - f2)/(numpy.exp(-t1/tau) - numpy.exp(-t2/tau))
-
-    def f(t: float) -> float:
-        return A + B*numpy.exp(-t/tau)
-
-    return f
-
-
-def _sin_edge_func(t1: float, f1: float, t2: float, f2: float) -> Callable[[float], float]:
-
-    fm = (f1+f2)/2
-    tm = (t1+t2)/2
-    T = 2*(t2-t1)
-    w = 2*numpy.pi/T
-    phi = w*tm
-    A = f2-fm
-
-    def f(t: float) -> float:
-        return fm + A*numpy.sin(w*t - phi)
-
-    return f
-
-
-def _smoothstep_edge_func(t1: float, f1: float, t2: float, f2: float) -> Callable[[float], float]:
-
-    Am = numpy.array([[1, t1, t1**2, t1**3],
-                      [1, t2, t2**2, t2**3],
-                      [0, 1, 2*t1, 3*t1**2],
-                      [0, 1, 2*t2, 3*t2**2]])
-    Bm = numpy.array([f1, f2, 0, 0])
-    A, B, C, D = numpy.linalg.solve(Am, Bm)
-
-    def f(t):
-        return A + B*t + C*t**2 + D*t**3
-
-    return f
+    This class defines an exception meant to be raised when any type of rounding or loss of precision that causes the time coordinates of a PWL object to not be strictly increasing.
+    """
 
 
 class PWL():
@@ -156,36 +122,6 @@ class PWL():
             raise TypeError(
                 f"Attribute 'verbose' should be a boolean but has type '{type(new_verbose).__name__}'.")
         self._verbose = new_verbose
-
-    def _add(self, t: float, x: float) -> None:
-
-        if len(self._t_list) >= 1 and t <= self._t_list[-1]:
-            raise PrecisionError(
-                f"Internal Python rounding caused the time coordinates to not be strictly increasing when adding points to {self._name}.")
-
-        if len(self._t_list) == len(self._x_list) < 2:
-            self._t_list.append(t)
-            self._x_list.append(x)
-        else:
-            self._add_with_redundancy_check(x, t)
-
-    def _add_with_redundancy_check(self, x: float, t: float):
-
-        t_n_1 = self._t_list[-1]
-        t_n_2 = self._t_list[-2]
-
-        x_n_1 = self._x_list[-1]
-        x_n_2 = self._x_list[-2]
-
-        last_m = (x_n_1 - x_n_2)/(t_n_1 - t_n_2)
-        new_m = (x - x_n_1)/(t - t_n_1)
-
-        if last_m == new_m:
-            self._t_list[-1] = t
-            self._x_list[-1] = x
-        else:
-            self._t_list.append(t)
-            self._x_list.append(x)
 
     def hold(self, duration: float) -> None:
 
@@ -324,24 +260,6 @@ class PWL():
         # Actual function
         self._lin_edge(target, duration, 0)
 
-    def _lin_edge(self, target: float, duration: float, n: int) -> None:
-
-        if self._verbose:
-            if n == 0:
-                print(
-                    f"{self._name}: Adding linear edge with target of {target} and duration of {duration}.")
-            else:
-                print(
-                    n*"    "+f"Adding linear edge with target of {target} and duration of {duration}.")
-
-        if len(self._t_list) == len(self._x_list) == 0:
-            if self._verbose:
-                print((n+1)*"    "+"Empty PWL object. Adding initial (0, 0) point.")
-            self._add(0, 0)
-
-        last_t = self._t_list[-1]
-        self._add(last_t+duration, target)
-
     def exp_edge(self, target: float, duration: float, tau: float, t_step: Optional[float] = None) -> None:
 
         # Check for nullable arguments
@@ -394,9 +312,57 @@ class PWL():
         f = _exp_edge_func(tau=tau, t1=last_t, t2=last_t +
                            duration, f1=last_x, f2=target)
 
-        for t in numpy.arange(last_t+t_step, last_t+duration, t_step):
+        for t in np.arange(last_t+t_step, last_t+duration, t_step):
             self._add(t, f(t))
 
+        self._add(last_t+duration, target)
+
+    def _add(self, t: float, x: float) -> None:
+
+        if len(self._t_list) >= 1 and t <= self._t_list[-1]:
+            raise PrecisionError(
+                f"Internal Python rounding caused the time coordinates to not be strictly increasing when adding points to {self._name}.")
+
+        if len(self._t_list) == len(self._x_list) < 2:
+            self._t_list.append(t)
+            self._x_list.append(x)
+        else:
+            self._add_with_redundancy_check(x, t)
+
+    def _add_with_redundancy_check(self, x: float, t: float):
+
+        t_n_1 = self._t_list[-1]
+        t_n_2 = self._t_list[-2]
+
+        x_n_1 = self._x_list[-1]
+        x_n_2 = self._x_list[-2]
+
+        last_m = (x_n_1 - x_n_2)/(t_n_1 - t_n_2)
+        new_m = (x - x_n_1)/(t - t_n_1)
+
+        if last_m == new_m:
+            self._t_list[-1] = t
+            self._x_list[-1] = x
+        else:
+            self._t_list.append(t)
+            self._x_list.append(x)
+
+    def _lin_edge(self, target: float, duration: float, n: int) -> None:
+
+        if self._verbose:
+            if n == 0:
+                print(
+                    f"{self._name}: Adding linear edge with target of {target} and duration of {duration}.")
+            else:
+                print(
+                    n*"    "+f"Adding linear edge with target of {target} and duration of {duration}.")
+
+        if len(self._t_list) == len(self._x_list) == 0:
+            if self._verbose:
+                print((n+1)*"    "+"Empty PWL object. Adding initial (0, 0) point.")
+            self._add(0, 0)
+
+        last_t = self._t_list[-1]
         self._add(last_t+duration, target)
 
     def sin_edge(self, target: float, duration: float, t_step: Optional[float] = None) -> None:
@@ -445,7 +411,7 @@ class PWL():
 
         f = _sin_edge_func(t1=last_t, t2=last_t+duration, f1=last_x, f2=target)
 
-        for t in numpy.arange(last_t+t_step, last_t+duration, t_step):
+        for t in np.arange(last_t+t_step, last_t+duration, t_step):
             self._add(t, f(t))
 
         self._add(last_t+duration, target)
@@ -497,7 +463,7 @@ class PWL():
         f = _smoothstep_edge_func(
             t1=last_t, t2=last_t+duration, f1=last_x, f2=target)
 
-        for t in numpy.arange(last_t+t_step, last_t+duration, t_step):
+        for t in np.arange(last_t+t_step, last_t+duration, t_step):
             self._add(t, f(t))
 
         self._add(last_t+duration, target)
@@ -525,22 +491,101 @@ class PWL():
         x_list = self._x_list
 
         with open(filename, "w") as file:
-            ti_str = numpy.format_float_scientific(
+            ti_str = np.format_float_scientific(
                 t_list[0], precision-1, unique=False, sign=False)
-            xi_str = numpy.format_float_scientific(
+            xi_str = np.format_float_scientific(
                 x_list[0], precision-1, unique=False, sign=True)
             file.write(f"{ti_str}    {xi_str}\n")
             last_t = ti_str
             for ti, xi in zip(t_list[1:], x_list[1:]):
-                ti_str = numpy.format_float_scientific(
+                ti_str = np.format_float_scientific(
                     ti, precision-1, unique=False, sign=False)
-                xi_str = numpy.format_float_scientific(
+                xi_str = np.format_float_scientific(
                     xi, precision-1, unique=False, sign=True)
                 if ti_str == last_t:
                     raise PrecisionError(
                         "The chosen precision level caused the written time coordinates to not be strictly increasing.")
                 file.write(f"{ti_str}    {xi_str}\n")
                 last_t = ti_str
+
+
+def _exp_edge_func(tau: float, t1: float, f1: float, t2: float, f2: float) -> Callable[[float], float]:
+    """== _exp_edge_func *(Private Function)* ==
+
+    Private function that generates a function of the following form:
+
+        f(t) = A + B*exp(-t/tau)
+
+    The constants `A` and `B` chosen such that the conditions following conditions are met:
+
+        f(t1) = f1
+        f(t2) = f2
+    """
+
+    A = (f1*np.exp(t1/tau) - f2*np.exp(t2/tau)) / \
+        (np.exp(t1/tau) - np.exp(t2/tau))
+    B = (f1 - f2)/(np.exp(-t1/tau) - np.exp(-t2/tau))
+
+    def f(t: float) -> float:
+        return A + B*np.exp(-t/tau)
+
+    return f
+
+
+def _sin_edge_func(t1: float, f1: float, t2: float, f2: float) -> Callable[[float], float]:
+    """== _sin_edge_func *(Private Function)* ==
+
+    Private function that generates a function of the following form:
+
+        f(t) = A*sin(w*t - phi)
+
+    The constants `A`, `w` and `phi` are chosen such that the following conditions are met:
+
+        f(t1) = f1
+        f(t2) = f2
+        f'(t1) = f'(t2) = 0
+
+    Due to the periodic nature of sine, infinite solutions exist. The solution with smallest `w` is chosen.
+    """
+
+    fm = (f1+f2)/2
+    tm = (t1+t2)/2
+    T = 2*(t2-t1)
+    w = 2*np.pi/T
+    phi = w*tm
+    A = f2-fm
+
+    def f(t: float) -> float:
+        return fm + A*np.sin(w*t - phi)
+
+    return f
+
+
+def _smoothstep_edge_func(t1: float, f1: float, t2: float, f2: float) -> Callable[[float], float]:
+    """== _smoothstep_edge_func *(Private Funtion)* ==
+
+    Private function that generates a function of the following form:
+
+        f(t) = A + B*t + C*t^2 + D*t^3
+
+    The constants `A`, `B`, `C` and `D` are chosen such that the following conditions are met:
+
+        f(t1) = f1
+        f(t2) = f2
+        f'(t1) = f'(t2) = 0
+    """
+
+    Am = np.array([[1, t1, t1**2, t1**3],
+                   [1, t2, t2**2, t2**3],
+                   [0, 1, 2*t1, 3*t1**2],
+                   [0, 1, 2*t2, 3*t2**2]])
+    Bm = np.array([f1, f2, 0, 0])
+    A, B, C, D = np.linalg.solve(Am, Bm)
+
+    def f(t):
+        return A + B*t + C*t**2 + D*t**3
+
+    return f
 
 
 if __name__ == "__main__":
