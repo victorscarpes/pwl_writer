7bd511c4328c0655e37062939bb221e1e109c746
victorscarpes <victorscarpes@gmail.com>
2023-06-23 15:24:48 +0200

    Added docstring

--- pwl_writer/__init__.py
+++ pwl_writer/__init__.py
@@ -1,3 +1,5 @@
+"""Package to generate PWL incrementally using little 'events' like rectangular pulses and exponential rising or falling edges."""
+
 from .pwl_writer import *
 
 __version__ = "1.0.0"
